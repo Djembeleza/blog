43d39498182515f061518334ac906e5016bf3ca6
Djembeleza <lkaola11@gmail.com>
2020-09-21 15:27:31 +0200

    bootstrap navbar

--- blog/static_cdn/scripts/script.js
+++ blog/static_cdn/scripts/script.js
@@ -1,4 +1,24 @@
 // DOM Elements
 const navList = document.getElementsByClassName('nav-list');
+const navBtn = document.getElementsByClassName('nav-btn');
 
-console.log(navList);
+
+// Hide the nav list if scripts are on
+navList[0].style.display = 'none';
+
+
+
+// Add Even Listener to Nav-Btn that toggles display properties
+navBtn[0].addEventListener("click", function () {
+    if (navList[0].style.display == 'none') {
+        navList[0].style.display = 'block';
+    } else {
+        navList[0].style.display = 'none';
+    }
+});
+
+
+
+
+
+// console.log(navList);
--- post/forms.py
+++ post/forms.py
@@ -1,20 +1,23 @@
 from django.contrib.auth.models import User
 from django import forms
 
+
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     subject = forms.CharField(max_length=50)
     emailAddress = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
 
-    
+
 class UserForm(forms.ModelForm):
     email2 = forms.EmailField(label="Confirm Email")
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'email2', 'password']
+        fields = ['username', 'first_name',
+                  'last_name', 'email', 'email2', 'password']
+
     def clean_email2(self):
         email = self.cleaned_data.get('email')
         email2 = self.cleaned_data.get('email2')
